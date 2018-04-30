193b9963c9c329eda3e4b62dd4d5d091ae70afff
juliaberk <jroseberk@gmail.com>
2018-04-30 02:32:09 +0000

    Flask beginnings

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
-from flask import Flask, request
-
+from flask import Flask
 app = Flask(__name__)
 
-# SET FALSE BEFORE PRODUCTION
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/")
+def hello():
+    return "Hello World!"

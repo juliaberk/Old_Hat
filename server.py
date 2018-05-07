df30796052451a70b445f65cab889d856ab72671
juliaberk <jroseberk@gmail.com>
2018-05-07 06:16:15 +0000

    Do I want to use Flask..s

--- server.py
+++ server.py
@@ -4,3 +4,12 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World!"
+
+# DEBUGGER STUFF ##########################################################
+
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the
+    # point that we invoke the DebugToolbarExtension
+    app.debug = True
+
+app.run(host="0.0.0.0")

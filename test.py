a82dc7aa0a9f16ec984174851f39e133f527facb
juliaberk <jroseberk@gmail.com>
2018-05-09 03:39:31 +0000

    Test Driven Development y'all

---
+++ test.py
@@ -0,0 +1,20 @@
+from server import append
+import unittest
+
+# class FlaskTests(TestCase):
+#
+#     def setUp(self):
+#         """Stuff to do before every test."""
+#
+#         self.client = app.test_client()
+#         app.config['TESTING'] = True
+#         connect_to_db(app, "postgresql:///testdb")
+#
+#         db.create_all()
+#         example_data()
+#
+#     def tearDown(self):
+#         """Do at end of every test."""
+#
+#         db.session.close()
+# db.drop_all()

54e9847df3412dbabe9fac5104e9df055b13e1a9
juliaberk <jroseberk@gmail.com>
2018-05-26 22:07:11 -0700

    Log In/Out

--- server.py
+++ server.py
@@ -4,6 +4,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World!"
+#
+Login/log out route
 
 # DEBUGGER STUFF ##########################################################
 
